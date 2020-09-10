6cdae9ba3abbe65211d4da5a7b9e50742b1183ad
developer@subhabrata <subhabrata.khatua@gmail.com>
2020-09-11 02:32:11 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import thunk from "redux-thunk";
 
 //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const devTools =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null;
+// const devTools =
+//   process.env.NODE_ENV === "development"
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+//       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+//     : null;
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
